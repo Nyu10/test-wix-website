af3a849498270c4c2cf2dedd23668953c68c05b1
Nehemiah Yu <nehemiahyu1u@gmail.com>
2021-06-03 22:51:02 -0400

    test

---
+++ Yanni/src/pages/Student_Projects.dyrlq/pageElements.d.ts
@@ -0,0 +1,21 @@
+type PageElementsMap = {
+  "#header1": $w.Header;
+  "#image2": $w.Image;
+  "#horizontalMenu1": $w.Menu;
+  "#accountNavBar1": $w.AccountNavBar;
+  "#container1": $w.HiddenCollapsedElement;
+  "#profile1": $w.IFrame;
+  "#horizontalMenu2": $w.Menu;
+  "#footer1": $w.Footer;
+  "#line3": $w.HiddenCollapsedElement;
+  "#text12": $w.Text;
+  "#wixForms1": $w.Node;
+  "#form2": $w.Form;
+  "#input5": $w.TextInput;
+  "#input4": $w.TextInput;
+  "#input6": $w.TextInput;
+  "#textBox2": $w.TextBox;
+  "#button5": $w.Button;
+  "#text16": $w.Text;
+  "#wixChat1": $w.Chatbox;
+}
